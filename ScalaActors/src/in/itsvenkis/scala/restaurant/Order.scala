b34e3724a59ff1684336da3d77661bba7d6a4656
Venkat Nanda <itsvenkis@gmail.com>
2015-02-04 00:48:00 +0530

    Added actors and orders

--- ScalaActors/src/in/itsvenkis/scala/restaurant/Order.scala
+++ ScalaActors/src/in/itsvenkis/scala/restaurant/Order.scala
@@ -3,8 +3,4 @@ package in.itsvenkis.scala.restaurant
 /**
  * Created by itsvenkis on 01/01/15.
  */
-case class Order(tableNumber:Int, dishName:String) {
-  
-   var spicy = false
-
-}
+case class Order(tableNumber:Int, dishName:String)
